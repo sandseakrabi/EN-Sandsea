689ad07e490a405e5fdd6f0ab924a6de56718c41
sandseakrabi <sandseakrabi@gmail.com>
2026-07-03 15:40:34 +0700

    sw.js

--- sw.js
+++ sw.js
@@ -4,7 +4,7 @@
 // (เช็คงานใหม่/อัปเดตสถานะ แม้แอปถูกปิดอยู่ — Android Chrome เท่านั้น)
 // ================================================================
 
-const GAS_URL   = 'https://script.google.com/macros/s/AKfycbwViAK68tokk2jPOEx5nkSyVuCIVLHVzMfsCRXM12Ea9I3fl58VOTgNWLiFFlFWE2827Q/exec';
+const GAS_URL   = 'https://script.google.com/macros/s/AKfycbxuqJXbcXJO8NQYxUuoWZpplmYdCqK1moqKc8UNPdzGAGi7GVz58Sgqea6FVO-sokY75A/exec';
 const ICON      = './icon-EN.png';
 const SYNC_TAG  = 'en-check-tickets';
 const DB_NAME   = 'en_sw_db';
